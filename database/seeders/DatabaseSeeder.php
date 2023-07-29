b9eee88d2a82d95bafef1fcff5cada08ebfaee84
Noriaki Fujimaru <houken@me.com>
2023-07-29 11:28:41 +0900

    顧客一覧

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Customer;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +17,8 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             ItemSeeder::class,
         ]);
+
+        Customer::factory(1000)->create();
 
         // \App\Models\User::factory(10)->create();
 
---
+++ resources/js/Pages/Customers/Index.vue
@@ -0,0 +1,66 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head, Link } from '@inertiajs/vue3';
+import FlashMessage from '@/Components/FlashMessage.vue';
+import Pagination from '@/Components/Pagination.vue';
+
+defineProps({
+    customers: Object
+})
+</script>
+
+<template>
+    <Head title="顧客一覧" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">顧客一覧</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                    <section class="text-gray-600 body-font">
+                        <div class="container px-5 py-2 mx-auto">
+                            <FlashMessage />
+                            <div class="flex w-full p-4 mx-auto mt-4 lg:w-2/3">
+                                <Link as="button" :href="route('customers.create')"
+                                    class="flex px-6 py-2 ml-auto text-white bg-indigo-500 border-0 rounded focus:outline-none hover:bg-indigo-600">
+                                顧客登録</Link>
+                            </div>
+                            <div class="w-full mx-auto overflow-auto lg:w-2/3">
+                                <table class="w-full text-left whitespace-no-wrap table-auto">
+                                    <thead>
+                                        <tr>
+                                            <th
+                                                class="px-4 py-2 text-sm font-medium tracking-wider text-gray-900 bg-gray-100 rounded-tl rounded-bl title-font">
+                                                id</th>
+                                            <th
+                                                class="px-4 py-2 text-sm font-medium tracking-wider text-gray-900 bg-gray-100 title-font">
+                                                氏名</th>
+                                            <th
+                                                class="px-4 py-2 text-sm font-medium tracking-wider text-gray-900 bg-gray-100 title-font">
+                                                カナ</th>
+                                            <th
+                                                class="px-4 py-2 text-sm font-medium tracking-wider text-gray-900 bg-gray-100 title-font">
+                                                電話</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr v-for="customer in customers.data" :key="customer.id">
+                                            <td class="px-4 py-1 text-sm border-b-2 border-gray-200">{{ customer.id }}</td>
+                                            <td class="px-4 py-1 text-sm border-b-2 border-gray-200">{{ customer.name }}</td>
+                                            <td class="px-4 py-1 text-sm border-b-2 border-gray-200">{{ customer.kana }}</td>
+                                            <td class="px-4 py-1 text-sm border-b-2 border-gray-200">{{ customer.tel }}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                <Pagination class="mt-6" :links="customers.links"></Pagination>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
