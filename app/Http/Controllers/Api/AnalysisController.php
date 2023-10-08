e62dcf5fce615503822d1be3a174985ecb1860df
Noriaki Fujimaru <houken@me.com>
2023-10-08 11:39:53 +0900

    perDay complete

--- app/Http/Controllers/Api/AnalysisController.php
+++ app/Http/Controllers/Api/AnalysisController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Order;
+use App\Services\AnalysisService;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\DB;
 
 class AnalysisController extends Controller
 {
@@ -15,19 +15,20 @@ class AnalysisController extends Controller
         $subQuery = Order::betweenDate($request->startDate, $request->endDate);
 
         if ($request->type === 'perDay') {
-            $subQuery
-                ->where('status', true)
-                ->groupBy('id')
-                ->selectRaw('SUM(subtotal) AS totalPerPurchase, DATE_FORMAT(created_at, "%Y%m%d") AS date')
-                ->groupBy('date');
+            list($data, $labels, $totals) = AnalysisService::perDay($subQuery);
+            // $subQuery
+            //     ->where('status', true)
+            //     ->groupBy('id')
+            //     ->selectRaw('SUM(subtotal) AS totalPerPurchase, DATE_FORMAT(created_at, "%Y%m%d") AS date')
+            //     ->groupBy('date');
 
-            $data = DB::table($subQuery)
-                ->groupBy('date')
-                ->selectRaw('date, sum(totalPerPurchase) as total')
-                ->get();
+            // $data = DB::table($subQuery)
+            //     ->groupBy('date')
+            //     ->selectRaw('date, sum(totalPerPurchase) as total')
+            //     ->get();
 
-            $labels = $data->pluck('date');
-            $totals = $data->pluck('total');
+            // $labels = $data->pluck('date');
+            // $totals = $data->pluck('total');
         }
 
         return response()->json([
